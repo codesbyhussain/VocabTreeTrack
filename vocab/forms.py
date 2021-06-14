a8c95cb10f26f7d04f0fc2fa7e80560bf5c78d84
Hussain Manasi <hussain.manasi@yahoo.com>
2021-06-14 21:58:24 +0530

    changes made

--- vocab/forms.py
+++ vocab/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms.models import ModelChoiceField
 from .models import Word
 from django.forms import ModelForm
 from django.utils.safestring import mark_safe
@@ -7,3 +8,5 @@ class wordform(ModelForm):
     class Meta:
         model = Word
         exclude = []
+
+    parent = ModelChoiceField(queryset = Word.objects.all())
