72dda7969aaae90f714663a67370c0a2ca081a3a
Hussain Manasi <58601158+codesbyhussain@users.noreply.github.com>
2021-06-14 22:24:51 +0530

    Removed key

--- VocabTree/settings.py
+++ VocabTree/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jsq^y(t9q%r6=3tx45=!#so(mk4#0n-6wr$kq6wq#wsg)end0*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
